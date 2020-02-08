48c7e5b4844cddf0dfa0039e5b6603d9fc35a625
SlickJacket <novenarii@gmail.com>
2020-02-08 12:09:00 -0500

    Updates

---
+++ precache-manifest.930fd6a5d3a81c938d7d8f45c2bbbf53.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12429adfc2be0efa95a35f1abe2051c1",
+    "url": "/bobbybuffaloboy/index.html"
+  },
+  {
+    "revision": "db68fc147b08de714c17",
+    "url": "/bobbybuffaloboy/static/css/main.c7f11bee.chunk.css"
+  },
+  {
+    "revision": "205a805b084222a08234",
+    "url": "/bobbybuffaloboy/static/js/2.13314106.chunk.js"
+  },
+  {
+    "revision": "b5c8281ca89c1ddb89c15ead0e00f32f",
+    "url": "/bobbybuffaloboy/static/js/2.13314106.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db68fc147b08de714c17",
+    "url": "/bobbybuffaloboy/static/js/main.92720994.chunk.js"
+  },
+  {
+    "revision": "ce0d3f47839cb889645c",
+    "url": "/bobbybuffaloboy/static/js/runtime-main.0aadfad2.js"
+  },
+  {
+    "revision": "3181af4d75ae03e2208d7017eeaed3b0",
+    "url": "/bobbybuffaloboy/static/media/Melius1.3181af4d.png"
+  },
+  {
+    "revision": "f51de274a3c8b70fd8e3df868ff18e9e",
+    "url": "/bobbybuffaloboy/static/media/Melius1gf.f51de274.gif"
+  },
+  {
+    "revision": "31dd690bcd313e759f9c1cd9a2e9b9a9",
+    "url": "/bobbybuffaloboy/static/media/Melius_Logo2.31dd690b.png"
+  },
+  {
+    "revision": "c0642ff2212a35b0c580ce11bd860012",
+    "url": "/bobbybuffaloboy/static/media/goldteethusalogo.c0642ff2.png"
+  },
+  {
+    "revision": "ba5ea3f19a5fd1b994672f203aeb24d2",
+    "url": "/bobbybuffaloboy/static/media/gtusagf.ba5ea3f1.gif"
+  },
+  {
+    "revision": "38753e6c7e7472005bdd608182480d39",
+    "url": "/bobbybuffaloboy/static/media/heart.38753e6c.png"
+  },
+  {
+    "revision": "67ab1d6e8612e97f71602fbe965185e5",
+    "url": "/bobbybuffaloboy/static/media/melius2gf.67ab1d6e.gif"
+  },
+  {
+    "revision": "961bcbebe2f14bb709211de9fb64338d",
+    "url": "/bobbybuffaloboy/static/media/profile.961bcbeb.png"
+  }
+]);
